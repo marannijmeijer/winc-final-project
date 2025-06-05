3930c384a1d0c97847a38f4df3ff8b4e202eeade
marannijmeijer <marannijmeijer@outlook.com>
2025-06-05 20:16:27 +0200

    Updated property query parameters

--- services/properties/getProperties.js
+++ services/properties/getProperties.js
@@ -3,16 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const getProperties = async (location, pricePerNight, amenities) => {
     const prisma = new PrismaClient()
 
-    prisma.location = location ?? prisma.location
-    prisma.pricePerNight = parseFloat(pricePerNight) ?? prisma.pricePerNight
-    prisma.amenities = amenities ?? prisma.amenities
+    const propertyValues = {
+        location,
+        amenities
+    }
+    if (pricePerNight) {
+        propertyValues.pricePerNight = parseFloat(pricePerNight)
+    }
 
     return await prisma.property.findMany({
-        where: {
-            location,
-            pricePerNight,
-            amenities
-        }
+        where: propertyValues
     })
 }
 
